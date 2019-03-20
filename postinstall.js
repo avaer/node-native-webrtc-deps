8851b3c1b1d47336529cd088d6a21d4765b0dd93
Avaer Kazmer <a@modules.io>
2019-03-20 01:41:09 -0400

    Use unzipper module instead of unzip

--- postinstall.js
+++ postinstall.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const fs = require('fs');
-const unzip = require('unzip');
+const unzipper = require('unzipper');
 fs.createReadStream(path.join(__dirname, 'lib.zip'))
-  .pipe(unzip.Extract({
+  .pipe(unzipper.Extract({
     path: __dirname,
   }));
